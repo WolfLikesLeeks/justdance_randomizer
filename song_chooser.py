993f8e6842a5cc2f66eca847cfa0a63c920ee85c
WolfLikesLeeks <68028226+WolfLikesLeeks@users.noreply.github.com>
2020-07-08 14:41:46 -0400

    Add files via upload
    Made some changes to song_scraper.py to replace unicode characters with spaces.  Otherwise there were errors and prevention of songs with apostrophes or non-English letters to be added. Also included url for Just Dance 2020 and third page of unlimited  songs.
    
    Made change to song_chooser.py to ask user for number of songs so that a playlist could be made.

--- song_chooser.py
+++ song_chooser.py
@@ -1,16 +1,24 @@
 import random
 from time import sleep
 
+number = int(input("How many songs? "))
 file = open('just_dance_list.csv', 'r')
 songRead = file.readlines()
 file.close()
+playlist = []
+for j in range(number):
+    for i in range(10):
+        songIndex = random.randint(0, len(songRead)-1)
+        songName = songRead[songIndex]
+        print('', end="\r")
+        print("RANDOMIZED SONG: {}".format(str(songName[:-1])), end="\r")
+        sleep(0.01)
+    playlist.append(songName)
 
-for i in range(100):
-    songIndex = random.randint(0, len(songRead)-1)
-    songName = songRead[songIndex]
-    print('', end="\r")
-    print("RANDOMIZED SONG: {}".format(str(songName[:-1])), end="\r")
-    sleep(0.01)
 print('!'*128)
-print('\n\n\n\t{}\n\n\n'.format(songName))
+print('\n\n\n')
+for x in playlist:
+    st = x + '\n'
+    print(st)
+print('\n\n\n')
 print('!'*128)
--- song_scraper.py
+++ song_scraper.py
@@ -4,35 +4,50 @@ from bs4 import BeautifulSoup
 # url = 'https://justdance.fandom.com/wiki/Just_Dance_Unlimited'
 url_1 = 'https://justdance.fandom.com/wiki/Category:Songs_in_Just_Dance_Unlimited'
 url_2 = 'https://justdance.fandom.com/wiki/Category:Songs_in_Just_Dance_Unlimited?from=Kurio+ko+uddah+le+jana'
-
-urls = [url_1, url_2]
-
+url_3 = 'https://justdance.fandom.com/wiki/Category:Songs_in_Just_Dance_Unlimited?from=There+Is+Nothing+Better+In+The+World+%28%D0%9D%D0%B8%D1%87%D0%B5%D0%B3%D0%BE+%D0%BD%D0%B0+%D1%81%D0%B2%D0%B5%D1%82%D0%B5+%D0%BB%D1%83%D1%87%D1%88%D0%B5+%D0%BD%D0%B5%D1%82%D1%83%29'
+url_4 = 'https://justdance.fandom.com/wiki/Category:Songs_in_Just_Dance_2020'
+bypass = []
+urls = [url_1, url_2, url_3, url_4]
+songList = []
 for url in urls:
     r = requests.get(url)
     data = r.text
+    data = data.encode('ascii', 'ignore')
     soup = BeautifulSoup(data, 'html.parser')
 
-    songList = []
+    
     for element in soup.find_all('a'):
         try:
             title = element['title']
-            if "Just Dance" in title:
+            if 'Just Dance' in title:
+                continue
+            elif 'Add new page' in title:
+                continue
+            elif 'Fandom' in title:
+                continue
+            elif 'Category:Songs' in title:
+                continue
+            elif 'Special:Categories' in title:
                 continue
             else:
                 if title not in songList:
-                    print("Appending {}".format(title))
-                    songList.append(title)
-                    if title == "Worth It":
+                    if title not in bypass:
+                        print("Appending {}".format(title))
+                        songList.append(title)
+                    else:
                         break
+                    
         except TypeError:
             continue
         except KeyError:
             continue
+##        except UnicodeEncodeError:
+##            continue
 
-
-    f = open('just_dance_list.csv', 'a')
-    for song in songList:
-        f.write(song + '\n')
-    f.close()
+songList.append("Just Dance") #For Lady Gaga's song
+f = open('just_dance_list.csv', 'a')
+for song in songList:
+    f.write(song + '\n')
+f.close()
 
 print("FINISHED SCRAPING AND WRITING")
